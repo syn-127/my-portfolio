d2b435e9e09c6a1bb686f38e16511360a7d71e82
clayton sampere <clayton.sampere@mvfglobal.com>
2023-05-01 00:40:39 +0100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,61 @@
+export const projects = [
+  {
+    title: "React Github Favourite Language",
+    subtitle: "React , Github API , Axios",
+    description:
+      "I made this for a interview test. It is a simple react app that uses the github api to get the favourite language of a user. It uses axios to make the api call and react to display the data.",
+    image: "./project-1.gif",
+    link: "https://github-favourite.claytonsampere.co.uk/",
+  },
+  {
+    title: "React Weather",
+    subtitle: "React and Axios",
+    description:
+      "I made a simple react app that uses the open weather api to get the weather for a city. It uses axios to make the api call and react to display the data. It also uses react hooks to manage the state.",
+    image: "./project-2.gif",
+    link: "https://weather.claytonsampere.co.uk/",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Clayton is a great developer, he is always willing to help and share his knowledge. He is a great person to work with.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Sooraj R",
+    company: "MVF Global",
+  },
+  {
+    quote:
+      "Clayton is good at finding the right audience for the right product. Really valuable asset to the team.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Julian A",
+    company: "666 Bet",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "Material UI",
+  "Facebook Marketing",
+  "Tailwind CSS",
+];
