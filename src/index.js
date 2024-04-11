74772762456789464aefb8c9fbded0fdd9f5fa43
clayton sampere <clayton.sampere@mvfglobal.com>
2024-04-11 19:08:48 +0100

    added sentry

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://a591c2a13c320c5c3a627af16c015ffa@o4507069169664000.ingest.us.sentry.io/4507069171695616",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/claytonsampere\.co.uk/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.render(
   <React.StrictMode>
